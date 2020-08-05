6dc6e24c545956e28cb58c78b9ad430ab7c87a2d
MT-TH <mathijs.vandepoel@gmail.com>
2020-08-05 10:22:37 +0200

    1 RT plotting OK

--- plottingSandbox.py
+++ plottingSandbox.py
@@ -3,31 +3,77 @@ import time
 import threading
 from itertools import count
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
 import random
 import numpy as np
+import time
+import datetime as dt
 
-import numpy as np
 import matplotlib.pyplot as plt
 
-# plotting
 plt.style.use('dark_background')
 
-plt.axis([0, 50, 0, 100])
 
-x_values = []
-y_values = []
+#  length of window
+n = 50
 
-for i in range(50):
-    x_values.append(i)
-    y_values.append(random.randint(0, 100))
-    plt.plot(x_values, y_values,'r')
-    plt.pause(0.05)
+# Create figure for plotting
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+xs = list(range(n))
+ys = [0]*n
 
+# This function is called periodically from FuncAnimation
+def animate(i, xs, ys):
+
+
+    # Add x and y to lists
+    # xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+    ys.append(random.randint(0, 100))
+
+    # Limit x and y lists to 'n' items
+    # xs = xs[-n:]
+    ys = ys[-n:]
+
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(xs, ys)
+
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('Fancy title')
+    plt.ylabel('Sun sensor (%)')
+    plt.axis([0,n+4,0,100])
+    plt.xticks([])
+
+# Set up plot to call animate() function periodically
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=500)
 plt.show()
 
-
+#  --------------------------------
 #
+# # plotting
+# plt.style.use('dark_background')
+#
+# plt.axis([0, 50, 0, 100])
+#
+# x_values = []
+# y_values = []
+#
+# for i in range(50):
+#     x_values.append(i)
+#     y_values.append(random.randint(0, 100))
+#     plt.plot(x_values, y_values,'r')
+#     plt.pause(0.05)
+#
+# #     for future
+#     timeAx = dt.datetime.now().strftime('%H:%M:%S.%f')
+#
+# plt.show()
+
+
+# ----------------------------------------------
 #
 # plt.style.use('fivethirtyeight')
 #
